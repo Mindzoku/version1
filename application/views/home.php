3e1b80163944c756221278e11afa37511775406b
Nuttapon Phannurat <n.phannurat@gmail.com>
2013-11-24 16:06:21 +0700

    gh

--- application/views/home.php
+++ application/views/home.php
@@ -9,16 +9,11 @@
         <div width="1024" height="350" align="middle"><iframe src="http://files.bannersnack.com/iframe/embed.html?hash=bt9s4mpp&bgcolor=%233D3D3D&wmode=opaque&t=1383461794" width="950" height="300" seamless="seamless" scrolling="no" frameborder="0" allowtransparency="true"></iframe></div>
     
     <!-- Categories -->
-    <div class="span3 bs-docs-sidebar" >
-        <ul class="nav nav-list bs-docs-sidenav">
-            <li class="nav-header">Categories</li>
-            <li><a href="#"><i class="icon-chevron-right" ></i>Backpack</a></li>
-            <li><a href="#"><i class="icon-chevron-right"></i>Briefcase</a></li>
-            <li><a href="#"><i class="icon-chevron-right"></i>Ipad Bags</a></li>
-            <li><a href="#"><i class="icon-chevron-right"></i>Hand Bags</a></li>
-        </ul>
-    </div>
+    <? $this->load->view('intro/categories'); ?>
+
+    
     <div class="span8">
+        <!-- Promotion -->
         <ul class="thumbnails">
           <li class="span4">
             PROMOTION1
@@ -29,7 +24,146 @@
             <img src="<? echo base_url(); ?>/assets/img/promotion/pro2.jpg" class="img-rounded">
           </li>
         </ul>
+
+        <!-- New Arrivals -->
+        <ul class="thumbnails" style="border-style:solid; border-width:1px; border-color:#C0C0C0;">
+            <p>&nbsp;&nbsp;<b>NEW ARRIVALS</b></p>
+            <li class="span3">
+                <div class="thumbnail">
+                <img src="<? echo base_url(); ?>/assets/img/new/new1.jpg" class="img-rounded">
+                <div class="caption">
+                    <h3>MZK-BE01-1</h3>
+                    <p>เป้ก็ได้ สะพายข้างก็ได้ เทรนใหม่ ดีไซน์กิ๊บเก๋
+                        น้ำหนักเบา ไม่หนักบ่า ด้านในเป็นซับในยีนส์ไม่ขาดง่าย แข็งแรงทนทาน
+                        สายสะพายเป็นเนื้อผ้าอย่างดี ไม่ลุ่ยแน่นอนค่ะ <br>
+                        Price : 1,250 THB
+                    </p><br><br>
+                    <!-- <input type="image" src="<? echo base_url(); ?>/assets/img/tocart.jpg" > -->
+                    <button class="btn btn-primary" >Add to Cart</button>
+                </div>
+                </div>
+            </li>
+            <li class="span3">
+                <div class="thumbnail">
+                <img src="<? echo base_url(); ?>/assets/img/new/new2.jpg" class="img-rounded">
+                <div class="caption">
+                    <h3>MZK-jean01</h3>
+                    <p>-กระเป๋าเข็มขัดกระดุมแม่เหล้ก ใส่ tablet ได้ ด้านในเป็นซับในผ้ากระสอบญี่ปุ่นหลายช่องให้เก็บของได้ กิ๊บเก๋ ไม่เหมือนใครคะ
+                        -ขนาด 11 นิ้ว* 10 นิ้ว
+                        -พีวีซีเกรดเอ อะไหล่เป็นงานซิงค์ไม่ขึ้นสนิม<br>
+                        Price : 790 บาทคะ
+                    </p><br>
+                    <button class="btn btn-primary" >Add to Cart</button>
+                </div>
+                </div>
+            </li>
+            <li class="span3">
+                <div class="thumbnail">
+                <img src="<? echo base_url(); ?>/assets/img/new/new3.jpg" class="img-rounded">
+                <div class="caption">
+                    <h3>MZK-BE02</h3>
+                    <p>เป้ก็ได้ สะพายข้างก็ได้ ใช้ได้หลากหลายสไตล์ น้ำหนักเบา ไม่หนักบ่า                           เป็นกระเป๋าใบเก่ง สุดชิค ตัวกระเป๋าเป็นผ้าแคนวาสลายตุ๊กตา
+                        ซับในลายสกอต สายสะพายเป็นเนื้อผ้าอย่างดี 
+                        ไม่ลุ่ยแน่นอนค่ะ ไม่ว่าจะวันเบาๆ หรือวันที่เร่งรีบ ใบด้วยเอาอยู่คะ<br>
+                        Price : 1250 THB
+                    </p>
+                    <button class="btn btn-primary" >Add to Cart</button>
+                </div>
+                </div>
+             </li>
+        </ul>
+
+        <!-- Recommend -->
+        <ul class="thumbnails" style="border-style:solid; border-width:1px; border-color:#C0C0C0;">
+            <p>&nbsp;&nbsp;<b>RECOMMEND</b></p>
+            <li class="span3">
+                <div class="thumbnail">
+                <img src="<? echo base_url(); ?>/assets/img/recommend/re1.jpg" class="img-rounded">
+                <div class="caption">
+                    <h3>MZK-BE04</h3>
+                    <p>กระเป๋า A4 เข็มขัดแม่เหล็กผ้ากระสอบญี่ปุ่น<br>
+                        price : 1,250 THB
+                    </p><br><br>
+                    <!-- <input type="image" src="<? echo base_url(); ?>/assets/img/tocart.jpg" > -->
+                    <button class="btn btn-primary" >Add to Cart</button>
+                </div>
+                </div>
+            </li>
+            <li class="span3">
+                <div class="thumbnail">
+                <img src="<? echo base_url(); ?>/assets/img/new/new2.jpg" class="img-rounded">
+                <div class="caption">
+                    <h3>MZK-jean01</h3>
+                    <p>-กระเป๋าเข็มขัดกระดุมแม่เหล้ก ใส่ tablet ได้ ด้านในเป็นซับในผ้ากระสอบญี่ปุ่นหลายช่องให้เก็บของได้ กิ๊บเก๋ ไม่เหมือนใครคะ
+                        -ขนาด 11 นิ้ว* 10 นิ้ว
+                        -พีวีซีเกรดเอ อะไหล่เป็นงานซิงค์ไม่ขึ้นสนิม<br>
+                        price : 790 บาทคะ
+                    </p><br>
+                    <button class="btn btn-primary" >Add to Cart</button>
+                </div>
+                </div>
+            </li>
+            <li class="span3">
+                <div class="thumbnail">
+                <img src="<? echo base_url(); ?>/assets/img/new/new3.jpg" class="img-rounded">
+                <div class="caption">
+                    <h3>MZK-BE02</h3>
+                    <p>เป้ก็ได้ สะพายข้างก็ได้ ใช้ได้หลากหลายสไตล์ น้ำหนักเบา ไม่หนักบ่า                           เป็นกระเป๋าใบเก่ง สุดชิค ตัวกระเป๋าเป็นผ้าแคนวาสลายตุ๊กตา
+                        ซับในลายสกอต สายสะพายเป็นเนื้อผ้าอย่างดี 
+                        ไม่ลุ่ยแน่นอนค่ะ ไม่ว่าจะวันเบาๆ หรือวันที่เร่งรีบ ใบด้วยเอาอยู่คะ<br>
+                        price : 1250 THB
+                    </p>
+                    <button class="btn btn-primary" >Add to Cart</button>
+                </div>
+                </div>
+             </li>
+
+             <li class="span3">
+                <div class="thumbnail">
+                <img src="<? echo base_url(); ?>/assets/img/recommend/re1.jpg" class="img-rounded">
+                <div class="caption">
+                    <h3>MZK-BE04</h3>
+                    <p>กระเป๋า A4 เข็มขัดแม่เหล็กผ้ากระสอบญี่ปุ่น<br>
+                        price : 1,250 THB
+                    </p><br><br>
+                    <!-- <input type="image" src="<? echo base_url(); ?>/assets/img/tocart.jpg" > -->
+                    <button class="btn btn-primary" >Add to Cart</button>
+                </div>
+                </div>
+            </li>
+            <li class="span3">
+                <div class="thumbnail">
+                <img src="<? echo base_url(); ?>/assets/img/new/new2.jpg" class="img-rounded">
+                <div class="caption">
+                    <h3>MZK-jean01</h3>
+                    <p>-กระเป๋าเข็มขัดกระดุมแม่เหล้ก ใส่ tablet ได้ ด้านในเป็นซับในผ้ากระสอบญี่ปุ่นหลายช่องให้เก็บของได้ กิ๊บเก๋ ไม่เหมือนใครคะ
+                        -ขนาด 11 นิ้ว* 10 นิ้ว
+                        -พีวีซีเกรดเอ อะไหล่เป็นงานซิงค์ไม่ขึ้นสนิม<br>
+                        price : 790 บาทคะ
+                    </p><br>
+                    <button class="btn btn-primary" >Add to Cart</button>
+                </div>
+                </div>
+            </li>
+            <li class="span3">
+                <div class="thumbnail">
+                <img src="<? echo base_url(); ?>/assets/img/new/new3.jpg" class="img-rounded">
+                <div class="caption">
+                    <h3>MZK-BE02</h3>
+                    <p>เป้ก็ได้ สะพายข้างก็ได้ ใช้ได้หลากหลายสไตล์ น้ำหนักเบา ไม่หนักบ่า                           เป็นกระเป๋าใบเก่ง สุดชิค ตัวกระเป๋าเป็นผ้าแคนวาสลายตุ๊กตา
+                        ซับในลายสกอต สายสะพายเป็นเนื้อผ้าอย่างดี 
+                        ไม่ลุ่ยแน่นอนค่ะ ไม่ว่าจะวันเบาๆ หรือวันที่เร่งรีบ ใบด้วยเอาอยู่คะ<br>
+                        price : 1250 THB
+                    </p>
+                    <button class="btn btn-primary" >Add to Cart</button>
+                </div>
+                </div>
+             </li>
+        </ul>
     </div>
+
+  
+
 
 
 
--- application/views/intro/header.php
+++ application/views/intro/header.php
@@ -1,5 +1,6 @@
 
-  
+
+	<meta charset="utf-8">  
   <!-- Bootstrap core CSS -->
     <link href="<? echo base_url(); ?>assets/css/bootstrap.css" rel="stylesheet">
     <link href="<? echo base_url(); ?>assets/css/docs.css" rel="stylesheet">
