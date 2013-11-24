8ea8da20d30370a4912513b2ef8657b3c4bd576d
dronflur <dronflur@gmail.com>
2013-11-24 16:08:52 +0700

    Revert "gh"
    This reverts commit 3750d11a28a7ee90eecd4042f670f94de2188304.

---
+++ application/views/admin/add-product.php
@@ -0,0 +1,56 @@
+<html>
+<head>
+    <? $this->load->view('header'); ?>
+    <title>Home</title>
+</head>
+
+<body>
+    <? $this->load->view('menu'); ?>
+    <div width="1024" height="300" align="middle">
+    	<img src="<?echo base_url();?>/assets/admin-img/header.jpg">
+    </div>
+
+	<!-- Catagories -->
+    <? $this->load->view('/admin/sidebar-catagory'); ?>
+
+    <div class="span8" style="margin-top: 2%;">
+    	<div><legend><b>Add Product</b></legend></div>
+
+    	<div class="row-fluid">
+    		<div class="span1"></div>
+		  	<div class="span3">Product Name: </div>
+		  	<div class="span4"> <input type="text" id="catagory-name" name="catagoryName"> </div>
+		</div>
+
+		<div class="row-fluid">
+    		<div class="span1"></div>
+		  	<div class="span3">Description: </div>
+		  	<div class="span4"> <textarea rows="3" id="description" name="description"></textarea> </div>
+		</div>
+
+        <div class="row-fluid">
+            <div class="span1"></div>
+            <div class="span3">Price: </div>
+            <div class="span4"> <input type="text" id="price" name="price"> </div>
+        </div>
+
+        <div class="row-fluid">
+            <div class="span1"></div>
+            <div class="span3">Quantity: </div>
+            <div class="span4"> <input type="number" id="quantity" name="quantity"> </div>
+        </div>
+
+		<div align="right"> 
+			<button class="btn btn-primary">submit</button>
+			<button class="btn">cancle</button>
+		</div>
+
+        
+    </div>
+
+
+
+
+    <? $this->load->view('footer'); ?>
+</body>
+</html>
